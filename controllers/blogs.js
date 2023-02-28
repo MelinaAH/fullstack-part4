c58234c81c62333074b8cf65cda6896d4ab1ba72
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-02-28 11:20:44 +0200

    4.2 step2 done

---
+++ controllers/blogs.js
@@ -0,0 +1,31 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const logger = require('../utils/logger');
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      logger.info(blogs);
+      response.json(blogs);
+    })
+    .catch(error => logger.error(error.message));
+});
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes
+  });
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result);
+    })
+    .catch(error => logger.error(error.message));
+});
+
+module.exports = blogsRouter;
--- index.js
+++ index.js
@@ -1,72 +1,8 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-require('dotenv').config();
-//const Blog = require('./models/blog');
-const mongoose = require('mongoose');
+//const http = require('http');
+const app = require('./app');
+const logger = require('./utils/logger');
+const config = require('./utils/config');
 
-mongoose.set('strictQuery', false);
-
-const mongoUrl = process.env.MONGODB_URI;
-
-console.log('connecting to', mongoUrl);
-mongoose.connect(mongoUrl)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('Error connecting to MongoDB:', error.message);
-  });
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  }
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      console.log(blogs);
-      response.json(blogs);
-    })
-    .catch(error => console.log(error.message));
-});
-
-app.post('/api/blogs', (request, response) => {
-  //const blog = new Blog(request.body);
-  const blog = new Blog({
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes
-  });
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    })
-    .catch(error => console.log(error.message));
-});
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
