0d5723415060103cb9157af8e843a69f23d15fbd
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-03-01 15:19:55 +0200

    4.7 step5 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -62,10 +62,51 @@ const mostBlogs = (blogs) => {
   };
 };
 
+const mostLikes = (blogs) => {
+  // Let's create an empty array on which
+  // the bloggers are collected
+  let bloggers = [];
+
+  blogs.forEach(blog => {
+    // adds a name of the blogger if it doesn't exist yet
+    if (!bloggers.includes(blog.author)) {
+      bloggers.push(blog.author);
+    }
+  });
+
+  // Let's create an array which includes the number of
+  // likes with the author's name
+  let bloggersAndNumberOfLikes = bloggers.map(blogger => {
+    // loops all blogs and counts the number of blogs
+    let numberOfLikes = 0;
+    blogs.forEach(blog => {
+      if (blog.author === blogger) {
+        numberOfLikes + blog.likes;
+      }
+    });
+
+    return { blogger: blogger, numberOfLikes };
+  });
+
+  // finds a blogger who has the most blogs
+  let maxLikesAuthor = bloggersAndNumberOfLikes[0];
+  bloggersAndNumberOfLikes.forEach(blogger => {
+    if (blogger.numberOfblogs > maxLikesAuthor.numberOfLikes) {
+      maxLikesAuthor = blogger;
+    }
+  });
+
+  return {
+    author: maxLikesAuthor.author,
+    likes: maxLikesAuthor.numberOfblogs
+  };
+};
+
 module.exports =
 {
   dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 };
