b4bbc688411fd9a06c1a7c29fe5fb23be4b2d3f5
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-03-01 11:35:38 +0200

    4.4 step2 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,14 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes;
+  };
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0);
+};
+
+module.exports = { dummy, totalLikes };
